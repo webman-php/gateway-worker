2d7d3019c61c5aaa30a23e722df6556039c65796
walkor <walkor@users.noreply.github.com>
2022-02-15 09:44:20 +0800

    upload

---
+++ src/BusinessWorker.php
@@ -0,0 +1,29 @@
+<?php
+namespace Webman\GatewayWorker;
+
+class BusinessWorker extends \GatewayWorker\BusinessWorker
+{
+    public function __construct($config)
+    {
+        foreach ($config as $key => $value)
+        {
+            $this->$key = $value;
+        }
+        $backtrace               = debug_backtrace();
+        $this->_autoloadRootPath = dirname($backtrace[0]['file']);
+    }
+
+    public function onWorkerStart()
+    {
+        $this->_onWorkerStart  = $this->onWorkerStart;
+        $this->_onWorkerReload = $this->onWorkerReload;
+        $this->_onWorkerStop = $this->onWorkerStop;
+        $this->onWorkerStop   = array($this, 'onWorkerStop');
+        $this->onWorkerStart   = array($this, 'onWorkerStart');
+        $this->onWorkerReload  = array($this, 'onWorkerReload');
+
+        $args = func_get_args();
+        $this->id = $args[0]->id;
+        parent::onWorkerStart();
+    }
+}
---
+++ src/Register.php
@@ -0,0 +1,33 @@
+<?php
+namespace Webman\GatewayWorker;
+
+class Register extends \GatewayWorker\Register
+{
+    public function __construct()
+    {
+        $backtrace               = debug_backtrace();
+        $this->_autoloadRootPath = dirname($backtrace[0]['file']);
+    }
+
+    public function onWorkerstart()
+    {
+        // 设置 onMessage 连接回调
+        $this->onConnect = array($this, 'onConnect');
+
+        // 设置 onMessage 回调
+        $this->onMessage = array($this, 'onMessage');
+
+        // 设置 onClose 回调
+        $this->onClose = array($this, 'onClose');
+
+        // 记录进程启动的时间
+        $this->_startTime = time();
+
+        // 强制使用text协议
+        $this->protocol = '\Workerman\Protocols\Text';
+
+        // reusePort
+        $this->reusePort = false;
+    }
+
+}
