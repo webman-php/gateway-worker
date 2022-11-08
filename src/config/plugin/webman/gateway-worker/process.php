b30ad4f142a8cecec1e50c2ffb4dbe9638f54a9a
mouyong <mouyong@users.noreply.github.com>
2022-11-08 09:03:01 +0800

    chore: change config order.
    更改配置顺序，注册中心 -> 网关 -> 业务处理进程

--- src/config/plugin/webman/gateway-worker/process.php
+++ src/config/plugin/webman/gateway-worker/process.php
@@ -5,9 +5,15 @@ use Webman\GatewayWorker\BusinessWorker;
 use Webman\GatewayWorker\Register;
 
 return [
-    'gateway' => [
+    'register' => [
+        'handler'     => Register::class,
+        'listen'      => 'text://0.0.0.0:7100',
+        'count'       => 1, // Must be 1
+        'constructor' => []
+    ],
+    'ws-gateway' => [
         'handler'     => Gateway::class,
-        'listen'      => 'websocket://0.0.0.0:7272',
+        'listen'      => 'websocket://0.0.0.0:7200',
         'count'       => cpu_count(),
         'reloadable'  => false,
         'constructor' => ['config' => [
@@ -15,23 +21,17 @@ return [
             'startPort'       => 2300,
             'pingInterval'    => 25,
             'pingData'        => '{"type":"ping"}',
-            'registerAddress' => '127.0.0.1:1236',
+            'registerAddress' => '127.0.0.1:7100',
             'onConnect'       => function(){},
         ]]
     ],
     'worker' => [
         'handler'     => BusinessWorker::class,
-        'count'       => cpu_count()*2,
+        'count'       => cpu_count() * 2,
         'constructor' => ['config' => [
             'eventHandler'    => plugin\webman\gateway\Events::class,
-            'name'            => 'ChatBusinessWorker',
-            'registerAddress' => '127.0.0.1:1236',
+            'name'            => 'BusinessBusinessWorker',
+            'registerAddress' => '127.0.0.1:7300',
         ]]
-    ],
-    'register' => [
-        'handler'     => Register::class,
-        'listen'      => 'text://0.0.0.0:1236',
-        'count'       => 1, // Must be 1
-        'constructor' => []
     ],
 ];
